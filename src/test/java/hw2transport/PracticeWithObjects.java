5c18d83ef61c5861cb0cc7518feeb23e8ada828c
Tatyana Moskvitina <moskvitinatanya429@gmail.com>
2022-10-23 11:58:07 +0300

    hw2 transport second update

--- src/test/java/hw2transport/PracticeWithObjects.java
+++ src/test/java/hw2transport/PracticeWithObjects.java
@@ -10,33 +10,16 @@ public class PracticeWithObjects {
     firstTransport.setVehicleType("Car");
     firstTransport.setFuelConsumption(10);
     firstTransport.setTankVolume(70);
-        /* carFC - eto fuel consumption/100 dlja transporta 1 ( car)
-           carTank - eto objem baka u transporta 1 (car)
-         */
-    double carFC = firstTransport.getFuelConsumption() / 100;
-    double carTank = firstTransport.getTankVolume();
-
 
     Transport secondTransport = new Transport();
     secondTransport.setVehicleType("Train");
     secondTransport.setFuelConsumption(200);
     secondTransport.setTankVolume(6350);
-         /* trainFC - eto fuel consumption/100 dlja transporta 2 ( train)
-           trainTank - eto objem baka u transporta 2 (train)
-         */
-    double trainFC = secondTransport.getFuelConsumption() / 100;
-    double trainTank = secondTransport.getTankVolume();
-
 
     Transport thirdTransport = new Transport();
     thirdTransport.setVehicleType("Plane");
     thirdTransport.setFuelConsumption(1700);
     thirdTransport.setTankVolume(90770);
-         /* plainFC  - eto fuel consumption/100 dlja transporta 3 (plane)
-           plainTank - eto objem baka u transporta 2 (plane
-           */
-    double planeFC = thirdTransport.getFuelConsumption() / 100;
-    double planeTank = thirdTransport.getTankVolume();
 
 
     Routes firstRoute = new Routes();
@@ -44,25 +27,18 @@ public class PracticeWithObjects {
     firstRoute.setStartPoint("Riga");
     firstRoute.setEndPoint("Ventspils");
     firstRoute.setDistance(198);
-    // distanceRouteOne - distancija marshruta 1
-    double distanceRouteOne = firstRoute.getDistance();
-
 
     Routes secondRoute = new Routes();
     secondRoute.setRouteName("Route2");
     secondRoute.setStartPoint("Tallinn");
     secondRoute.setEndPoint("Budapest");
     secondRoute.setDistance(1830);
-    //distanceRouteTwo - distancija marshruta 2
-    double distanceRouteTwo = secondRoute.getDistance();
 
     Routes thirdRoute = new Routes();
     thirdRoute.setRouteName("Route3");
     thirdRoute.setStartPoint("Moscow");
     thirdRoute.setEndPoint("Vladivostok");
     thirdRoute.setDistance(9162);
-    //distanceRouteThree - distancija marshruta 3
-    double distanceRouteThree = thirdRoute.getDistance();
 
 
     //-------------------VIVOD V KONSOLJ-----------------
@@ -70,11 +46,9 @@ public class PracticeWithObjects {
     System.out.println("Transport1+Route1:");
     System.out.println(firstTransport.getVehicleType());
     System.out.println(firstRoute.getStartPoint() + "-" + firstRoute.getEndPoint());
-        /*
-         carLitresForRouteOne - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
-        */
-    double carLitresForRouteOne = carFC * distanceRouteOne;
-    if (carTank < carLitresForRouteOne) {
+    // carLitresForRouteOne - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
+    double carLitresForRouteOne = firstTransport.getFuelConsumption() / 100 * firstRoute.getDistance();
+    if (firstTransport.getTankVolume() < carLitresForRouteOne) {
         System.out.println("don't pass the route on one fuel tank");
     } else {
         System.out.println("pass the route on one fuel tank");
@@ -84,11 +58,9 @@ public class PracticeWithObjects {
     System.out.println("Transport1+Route2:");
     System.out.println(firstTransport.getVehicleType());
     System.out.println(secondRoute.getStartPoint() + "-" + secondRoute.getEndPoint());
-        /*
-         carLitresForRouteTwo - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
-        */
-    double carLitresForRouteTwo = carFC * distanceRouteTwo;
-    if (carTank < carLitresForRouteTwo) {
+    //carLitresForRouteTwo - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
+    double carLitresForRouteTwo = firstTransport.getFuelConsumption() / 100 * secondRoute.getDistance();
+    if (firstTransport.getTankVolume() < carLitresForRouteTwo) {
         System.out.println("don't pass the route on one fuel tank");
     } else {
         System.out.println("pass the route on one fuel tank");
@@ -98,11 +70,9 @@ public class PracticeWithObjects {
     System.out.println("Transport1+Route3:");
     System.out.println(firstTransport.getVehicleType());
     System.out.println(thirdRoute.getStartPoint() + "-" + thirdRoute.getEndPoint());
-        /*
-         carLitresForRouteThree - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
-        */
-    double carLitresForRouteThree = carFC * distanceRouteThree;
-    if (carTank < carLitresForRouteThree) {
+    //carLitresForRouteThree - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
+    double carLitresForRouteThree = firstTransport.getFuelConsumption() / 100 * thirdRoute.getDistance();
+    if (firstTransport.getTankVolume() < carLitresForRouteThree) {
         System.out.println("don't pass the route on one fuel tank");
     } else {
         System.out.println("pass the route on one fuel tank");
@@ -112,11 +82,9 @@ public class PracticeWithObjects {
     System.out.println("Transport2+Route1:");
     System.out.println(secondTransport.getVehicleType());
     System.out.println(firstRoute.getStartPoint() + "-" + firstRoute.getEndPoint());
-        /*
-         trainLitresForRouteOne - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
-        */
-    double trainLitresForRouteOne = trainFC * distanceRouteOne;
-    if (trainTank < trainLitresForRouteOne) {
+    //trainLitresForRouteOne - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
+    double trainLitresForRouteOne = secondTransport.getFuelConsumption() / 100 * firstRoute.getDistance();
+    if (secondTransport.getTankVolume() < trainLitresForRouteOne) {
         System.out.println("don't pass the route on one fuel tank");
     } else {
         System.out.println("pass the route on one fuel tank");
@@ -126,11 +94,9 @@ public class PracticeWithObjects {
     System.out.println("Transport2+Route2:");
     System.out.println(secondTransport.getVehicleType());
     System.out.println(secondRoute.getStartPoint() + "-" + secondRoute.getEndPoint());
-        /*
-        trainLitresForRouteTwo - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
-         */
-    double trainLitresForRouteTwo = trainFC * distanceRouteTwo;
-    if (trainTank < trainLitresForRouteTwo) {
+        //trainLitresForRouteTwo - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
+    double trainLitresForRouteTwo = secondTransport.getFuelConsumption() / 100 * secondRoute.getDistance();
+    if (secondTransport.getTankVolume() < trainLitresForRouteTwo) {
         System.out.println("don't pass the route on one fuel tank");
     } else {
         System.out.println("pass the route on one fuel tank");
@@ -139,11 +105,9 @@ public class PracticeWithObjects {
     System.out.println("Transport2+Route3:");
     System.out.println(secondTransport.getVehicleType());
     System.out.println(thirdRoute.getStartPoint() + "-" + thirdRoute.getEndPoint());
-        /*
-        trainLitresForRouteThree - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
-        */
-    double trainLitresForRouteThree = trainFC * distanceRouteThree;
-    if (trainTank < trainLitresForRouteThree) {
+    // trainLitresForRouteThree - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
+    double trainLitresForRouteThree = secondTransport.getFuelConsumption() / 100 * thirdRoute.getDistance();
+    if (secondTransport.getTankVolume() < trainLitresForRouteThree) {
         System.out.println("don't pass the route on one fuel tank");
     } else {
         System.out.println("pass the route on one fuel tank");
@@ -153,11 +117,9 @@ public class PracticeWithObjects {
     System.out.println("Transport3+Route1:");
     System.out.println(thirdTransport.getVehicleType());
     System.out.println(firstRoute.getStartPoint() + "-" + firstRoute.getEndPoint());
-        /*
-        planeLitresForRouteOne - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
-        */
-    double planeLitresForRouteOne = planeFC * distanceRouteOne;
-    if (planeTank < planeLitresForRouteOne) {
+    // planeLitresForRouteOne - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
+    double planeLitresForRouteOne = thirdTransport.getFuelConsumption() / 100 * firstRoute.getDistance();
+    if (thirdTransport.getTankVolume() < planeLitresForRouteOne) {
         System.out.println("don't pass the route on one fuel tank");
     } else {
         System.out.println("pass the route on one fuel tank");
@@ -166,11 +128,9 @@ public class PracticeWithObjects {
     System.out.println("Transport3+Route2:");
     System.out.println(thirdTransport.getVehicleType());
     System.out.println(secondRoute.getStartPoint() + "-" + secondRoute.getEndPoint());
-        /*
-        planeLitresForRouteTwo - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
-        */
-    double planeLitresForRouteTwo = planeFC * distanceRouteTwo;
-    if (planeTank < planeLitresForRouteTwo) {
+    //planeLitresForRouteTwo - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
+    double planeLitresForRouteTwo = thirdTransport.getFuelConsumption() / 100 * secondRoute.getDistance();
+    if (thirdTransport.getTankVolume() < planeLitresForRouteTwo) {
         System.out.println("don't pass the route on one fuel tank");
     } else {
         System.out.println("pass the route on one fuel tank");
@@ -179,11 +139,9 @@ public class PracticeWithObjects {
     System.out.println("Transport3+Route3:");
     System.out.println(thirdTransport.getVehicleType());
     System.out.println(thirdRoute.getStartPoint() + "-" + thirdRoute.getEndPoint());
-        /*
-        planeLitresForRouteThree - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
-        */
-    double planeLitresForRouteThree = planeFC * distanceRouteThree;
-    if (planeTank < planeLitresForRouteThree) {
+    //planeLitresForRouteThree - eto kol-vo litrov, neobxodimoe transportu dlja proezda marshruta
+    double planeLitresForRouteThree = thirdTransport.getFuelConsumption() / 100 * thirdRoute.getDistance();
+    if (thirdTransport.getTankVolume() < planeLitresForRouteThree) {
         System.out.println("don't pass the route on one fuel tank");
     } else {
         System.out.println("pass the route on one fuel tank");
