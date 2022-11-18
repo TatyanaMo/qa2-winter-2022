648014255dfd388c81ce6a43f5fd8e4427143d4f
Tatyana Moskvitina <moskvitinatanya429@gmail.com>
2022-11-18 19:41:24 +0200

    choosing one book from list, find rating and review of book

--- src/test/java/hw4workingwithamazon/TestWithBook.java
+++ src/test/java/hw4workingwithamazon/TestWithBook.java
@@ -21,18 +21,17 @@ public class TestWithBook {
     private final String NAVIGATION_SHOP_MENU_ITEM = "Best Sellers";
     private final String BEST_SELLERS_CATEGORY = "Books";
 
-
     private final By ACCEPT_COOKIES_BTN = By.id("sp-cc-accept");
     private final By INNER_BTN = By.xpath(".//span[@class = 'a-button-inner']");
     private final By NAVIGATION_SHOP_MENU = By.xpath(".//div[@class = 'nav-progressive-content']/a");
     private final By BEST_SELLERS_CATEGORIES = By.xpath(".//div [@class = '_p13n-zg-nav-tree-all_style_zg-browse-item__1rdKf _p13n-zg-nav-tree-all_style_zg-browse-height-small__nleKL']/a");
-    private final By FOURTH_BOOK = By.id("p13n-asin-index-3");
-    private final By FOURTH_BOOK_COMMENT_LINK = By.xpath(".//div[@class = 'a-icon-row']/a");
+    private final By LIST_OF_BOOKS = By.xpath(".//div[@class =  'a-cardui _cDEzb_grid-cell_1uMOS p13n-grid-content']");
+    private final By BOOK_TO_OPEN_COMMENT_LINK = By.xpath(".//div[@class = 'a-icon-row']/a");
+    private final By RATING_ONE = By.xpath(".//span[@class = 'a-size-small']");
 
-    private final By RATING_ONE = By.xpath(".//span[@class = 'a-icon-alt']");
-   // private final By COMMENTS = By.xpath(".//span[@class = 'a-size-small']");
-
-    private final By RATING_TWO = By.xpath(".//span[@class = 'a-size-medium a-color-base']");
+    private final By STARS = By.xpath(".//span[@class = 'a-size-medium a-color-base']");
+    private final By RATING_TWO = By.xpath(".//span[@class = 'a-size-base a-color-secondary']");
+    private final By BOOK_REVIEW = By.xpath(".//div[@data-hook = 'cr-filter-info-review-rating-count']");
 
     @Test
     public void openHomePage() {
@@ -52,8 +51,8 @@ public class TestWithBook {
         List<WebElement> navigationShopMenuItems = browser.findElements(NAVIGATION_SHOP_MENU);
             for (WebElement menuItem : navigationShopMenuItems) {
                 if (menuItem.getText().equals(NAVIGATION_SHOP_MENU_ITEM)) {
-                menuItem.click();
-                break;
+                    menuItem.click();
+                    break;
                 }
             }
 
@@ -64,17 +63,24 @@ public class TestWithBook {
                     break;
                 }
             }
+        List<WebElement> books = browser.findElements(LIST_OF_BOOKS);
+            books.get(3);
+                String stars = books.get(3).findElement(BOOK_TO_OPEN_COMMENT_LINK).getAttribute("title");
+                System.out.println(stars);
 
-        WebElement fourthBook = browser.findElement(FOURTH_BOOK);
-            WebElement stars = fourthBook.findElement(RATING_ONE);
-                System.out.println(stars.getText());
+                WebElement rating = books.get(3).findElement(RATING_ONE);
+                System.out.println("rating" + " " + rating.getText());
 
-            //WebElement comments = fourthBook.findElement(COMMENTS);
-            //System.out.println(comments);
+                books.get(3).findElement(BOOK_TO_OPEN_COMMENT_LINK).click();
 
-            fourthBook.findElement(FOURTH_BOOK_COMMENT_LINK).click();
+                String bookStars = browser.findElement(STARS).getText();
+// System.out.println(stars.equals(bookStars));  // esli bil bi odinakovij tekst, mozhno bilo bi ispoljzovatj etot metod
+                System.out.println(bookStars +" " + "stars");
 
-               String bookRating = browser.findElement(RATING_TWO).getText();
-               System.out.println(bookRating);
+                String bookRating = browser.findElement(RATING_TWO).getText();
+                System.out.println(bookRating);
+
+                    String bookReview = browser.findElement(BOOK_REVIEW).getText();
+                    System.out.println(bookReview);
     }
    }
