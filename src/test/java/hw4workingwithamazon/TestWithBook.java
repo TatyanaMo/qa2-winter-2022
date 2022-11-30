515a51075352bc400fec7f7d022f333bc24c628b
Tatyana Moskvitina <moskvitinatanya429@gmail.com>
2022-11-30 07:59:10 +0200

    choosing one book from the list and counting comments

--- src/test/java/hw4workingwithamazon/TestWithBook.java
+++ src/test/java/hw4workingwithamazon/TestWithBook.java
@@ -1,15 +1,17 @@
 package hw4workingwithamazon;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.swing.text.StyledEditorKit;
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestWithBook {
@@ -18,11 +20,9 @@ public class TestWithBook {
     private WebDriverWait wait;
 
     private final String HOME_PAGE_URL = "http://www.amazon.de/";
-    private final String NAVIGATION_SHOP_MENU_ITEM = "Best Sellers";
-    private final String BEST_SELLERS_CATEGORY = "Books";
 
     private final By ACCEPT_COOKIES_BTN = By.id("sp-cc-accept");
-    private final By INNER_BTN = By.xpath(".//span[@class = 'a-button-inner']");
+    private final By CONTINUE_BTN = By.xpath(".//input[@data-action-type = 'DISMISS']");
     private final By NAVIGATION_SHOP_MENU = By.xpath(".//div[@class = 'nav-progressive-content']/a");
     private final By BEST_SELLERS_CATEGORIES = By.xpath(".//div [@class = '_p13n-zg-nav-tree-all_style_zg-browse-item__1rdKf _p13n-zg-nav-tree-all_style_zg-browse-height-small__nleKL']/a");
     private final By LIST_OF_BOOKS = By.xpath(".//div[@class =  'a-cardui _cDEzb_grid-cell_1uMOS p13n-grid-content']");
@@ -33,54 +33,85 @@ public class TestWithBook {
     private final By RATING_TWO = By.xpath(".//span[@class = 'a-size-base a-color-secondary']");
     private final By BOOK_REVIEW = By.xpath(".//div[@data-hook = 'cr-filter-info-review-rating-count']");
 
-    @Test
+    private final By REVIEW_LIST = By.xpath(".//div[@data-hook = 'review']");
+    private final By NEXT_PAGE_BTN = By.xpath(".//li[@class = 'a-last']/a");
+
+    @BeforeEach
     public void openHomePage() {
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
         browser = new ChromeDriver();
         browser.manage().window().maximize();
         browser.get(HOME_PAGE_URL);
         browser.findElement(ACCEPT_COOKIES_BTN).click();
+        browser.findElement(CONTINUE_BTN).click();
+    }
 
-        List<WebElement> shippingCountryCheckBtns = browser.findElements(INNER_BTN);
-        for (WebElement btn : shippingCountryCheckBtns) {
-            if (btn.getText().equals("CONTINUE")) {
-                btn.click();
+    @Test
+    public void selectionBook() {
+        //Test Data
+        String navigationShopMenuItem = "Best Sellers";
+        String bestSellersCategory = "Books";
+
+        List<WebElement> navigationShopMenuItems = browser.findElements(NAVIGATION_SHOP_MENU);
+        for (WebElement menuItem : navigationShopMenuItems) {
+            if (menuItem.getText().equals(navigationShopMenuItem)) {
+                menuItem.click();
                 break;
             }
         }
-        List<WebElement> navigationShopMenuItems = browser.findElements(NAVIGATION_SHOP_MENU);
-            for (WebElement menuItem : navigationShopMenuItems) {
-                if (menuItem.getText().equals(NAVIGATION_SHOP_MENU_ITEM)) {
-                    menuItem.click();
-                    break;
-                }
-            }
-
         List<WebElement> bestSellersCategories = browser.findElements(BEST_SELLERS_CATEGORIES);
-            for (WebElement category : bestSellersCategories) {
-                if (category.getText().equals(BEST_SELLERS_CATEGORY)) {
-                    category.click();
-                    break;
-                }
+        for (WebElement category : bestSellersCategories) {
+            if (category.getText().equals(bestSellersCategory)) {
+                category.click();
+                break;
             }
+        }
         List<WebElement> books = browser.findElements(LIST_OF_BOOKS);
-            books.get(3);
-                String stars = books.get(3).findElement(BOOK_TO_OPEN_COMMENT_LINK).getAttribute("title");
-                System.out.println(stars);
+        books.get(3);
+        String stars = books.get(3).findElement(BOOK_TO_OPEN_COMMENT_LINK).getAttribute("title");
+        System.out.println(stars);
 
-                WebElement rating = books.get(3).findElement(RATING_ONE);
-                System.out.println("rating" + " " + rating.getText());
+        String rating = books.get(3).findElement(RATING_ONE).getText();
+        System.out.println("rating" + " " + rating);
 
-                books.get(3).findElement(BOOK_TO_OPEN_COMMENT_LINK).click();
+        books.get(3).findElement(BOOK_TO_OPEN_COMMENT_LINK).click();
 
-                String bookStars = browser.findElement(STARS).getText();
-// System.out.println(stars.equals(bookStars));  // esli bil bi odinakovij tekst, mozhno bilo bi ispoljzovatj etot metod
-                System.out.println(bookStars +" " + "stars");
+        String bookStars = browser.findElement(STARS).getText();
+        System.out.println("book stars are equal: " + stars.contains(bookStars));   // sravnivaet tekst po chastichnomu sovpadeniju
 
-                String bookRating = browser.findElement(RATING_TWO).getText();
-                System.out.println(bookRating);
+        String bookRating = browser.findElement(RATING_TWO).getText();
+        System.out.println("book rating is equal: " + bookRating.contains(rating)); // sravnivaet tekst po chastichnomu sovpadeniju
 
-                    String bookReview = browser.findElement(BOOK_REVIEW).getText();
-                    System.out.println(bookReview);
+        String bookReview = browser.findElement(BOOK_REVIEW).getText();
+        System.out.println(bookReview);
+
+        List<WebElement> reviews = browser.findElements(REVIEW_LIST);
+        String idFirstReview = reviews.get(0).getAttribute("id");
+        try {
+            browser.findElement(NEXT_PAGE_BTN).click();
+        } catch (NoSuchElementException e) {
+            System.out.println(reviews.size());
+        }
+        while (browser.findElement(NEXT_PAGE_BTN).isEnabled()) {
+            wait = new WebDriverWait(browser, Duration.ofSeconds(10));
+            List<WebElement> reviewNextPage = new ArrayList<>(browser.findElements(REVIEW_LIST));
+            for (WebElement reviewNextPageId: reviewNextPage) {
+                System.out.println(reviewNextPageId.getAttribute("id")); // v konsole vidno chto id ne menjajutsja na stranicah, a dolzhni menjatsja
+            }
+            //List<WebElement> reviewNextPage = browser.findElements(REVIEW_LIST);
+            String nextPageIdFirstReview = reviewNextPage.get(0).getAttribute("id");
+            if (idFirstReview.equals(nextPageIdFirstReview)) {
+                wait = new WebDriverWait(browser, Duration.ofSeconds(10));
+                System.out.println("if " + reviews.size());
+            } else {
+                reviews.addAll(reviewNextPage);
+                System.out.println("else " + reviews.size());
+            }
+            browser.findElement(NEXT_PAGE_BTN).click();
+        }
     }
+    /*@AfterEach
+    public void closeBrowser() {
+        browser.close();
+    }*/
    }
